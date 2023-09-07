6a89960d98e87c2cbc27dd377dfc9388d0ade6cf
Clara <10088152+claraj@users.noreply.github.com>
2023-09-07 11:23:58 -0500

    Set new API URL

--- magic_8_ball/original_magic.py
+++ magic_8_ball/original_magic.py
@@ -2,7 +2,7 @@ import requests
 
 question = input('Enter your question for the magic 8 ball: ')
 
-magic_8_ball_url = f'https://magic-eight-ball.azurewebsites.net/magic/JSON/{question}'
+magic_8_ball_url = f'https://magic-8-ball-mctc.uc.r.appspot.com/magic/JSON/{question}'
 
 response = requests.get(magic_8_ball_url).json()
 
